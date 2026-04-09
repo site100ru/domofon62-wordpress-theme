b14ff2dcf04dda9886b3e15232d3a83921720274
Slava Sidorov <sidorov-vv3@mail.ru>
2026-04-09 11:09:51 +0500

    убрал из новой формы заголовок

--- footer-2.php
+++ footer-2.php
@@ -155,7 +155,6 @@
             <div class="popups modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h2 class="product-modal-title" id="checkConnectionModalLabel">Проверить подключение:</h2>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
