883f95f96afc69104916b1c366268a230fc649fa
SlavaWebDeveloper <sidorov-vv3@mail.ru>
2026-04-19 15:32:12 +0300

    обновил страницу с формой. убрал крестик и добавил паддинг по краям

--- page-form.php
+++ page-form.php
@@ -11,10 +11,10 @@
 <main class="light-grey-bg">
 	<div class="form-on-the-page">
 		<div class="popups modal-dialog modal-dialog-centered">
-			<div class="modal-content">
+			<div class="modal-content px-3">
 				<div class="modal-header">
                     <h2 class="product-modal-title" id="dostupModalLabel">Для получения доступа к услугам заполните форму:</h2>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
                 </div>
 				<div class="modal-body">
 					<div class="text-center"><?php echo do_shortcode('[contact-form-7 id="373" title="Форма получить доступ"]'); ?></div>  
