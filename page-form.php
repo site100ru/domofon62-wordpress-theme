cef79b922b2fba08b7650928ddc1542050afc22e
SlavaWebDeveloper <sidorov-vv3@mail.ru>
2026-05-21 18:06:55 +0500

    новая форма

--- page-form.php
+++ page-form.php
@@ -7,17 +7,16 @@
 	get_header( '4' );
 	
  ?>
- 
+
 <main class="light-grey-bg">
 	<div class="form-on-the-page">
 		<div class="popups modal-dialog modal-dialog-centered">
 			<div class="modal-content px-3">
 				<div class="modal-header">
-                    <h2 class="product-modal-title" id="dostupModalLabel">Для получения доступа к услугам заполните форму:</h2>
-                    <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
-                </div>
+					<h2 class="product-modal-title" id="dostupModalLabel">Получить доступ к услугам:</h2>
+				</div>
 				<div class="modal-body">
-					<div class="text-center"><?php echo do_shortcode('[contact-form-7 id="373" title="Форма получить доступ"]'); ?></div>  
+					<div class="text-center"><?php echo do_shortcode('[contact-form-7 id="373" title="Форма получить доступ"]'); ?></div>
 				</div>
 			</div>
 		</div>
